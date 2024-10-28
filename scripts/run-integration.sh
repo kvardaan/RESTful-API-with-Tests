1e9728ed0e209d5f3f254f4c0568dacc286c1281
kvardaan <vardaankhattar8907@gmail.com>
2024-10-28 21:14:26 +0530

    fix: test action

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,6 +1,7 @@
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
-./wait-for-it.sh "postgresql://postgres:password@localhost:5432/rest-test" -- echo '🟢 - Database is ready!'
+./scripts/wait-for-it.sh "postgresql://postgres:password@localhost:5432/rest-test" -- echo '🟢 - Database is ready!'
+npm install
 npm run db:migrate:dev
 npm run db:seed
 npm run test auth
